284422a0be70c6b400609d4c10b972ab34e1a4c1
Oleksandr Varga <vargasasha@gmail.com>
2023-05-05 15:42:51 +0300

    set connectios

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+console.log(axios);
+
+console.log(Notiflix);
+
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const btn = document.querySelector('.js-load-more');
+
+console.log(form);
+
+console.log(gallery);
+
+console.log(btn);
